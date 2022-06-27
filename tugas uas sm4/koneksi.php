306fd1e9c18755096397f2331a28f5e24097d37c
Annila Nurul H <80612405+ann541@users.noreply.github.com>
2022-06-27 10:06:38 +0700

    Add files via upload

---
+++ tugas uas sm4/koneksi.php
@@ -0,0 +1,14 @@
+<?php
+$conn_str = "host=abul.db.elephantsql.com " .
+    "port=5432 " .
+    "user=fbqhvyys " .
+    "dbname=fbqhvyys " .
+    "password=Sicp9ibK9ae7nQeT9eedNmQFh5ZABJjH";
+$conn = pg_connect($conn_str);
+
+// if($conn) {
+//     echo "<h3>Koneksi Berhasil</h3>";
+// } else {
+//     echo "<h3>Koneksi GAGAL TERSAMBUNG</h3>";
+// }
+?>
